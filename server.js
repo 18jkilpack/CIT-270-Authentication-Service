42977b4c4fb71db3ea8424efba5ae41e75091830
Jackson Kilpack <18jkilpack@gmail.com>
2022-09-30 13:13:24 -0600

    adding various files

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+const {v4 : uuidv4} = require ("uuid");
+const port = 3000;
+const app = express();
+const {createClient} = require('redis');
+const md5 = require('md5');
+const redisClient = createClient(
+    {
+    Url:'redis://default@localhost:6379',
+    }
+);
+
+app.use(bodyParser.json());
+
+app.listen(port, async ()=>{
+    await redisClient.connect();
+    console.log('Listening on port: ',port);
+});
+
+
+app.get("/", (req,res)=>{
+    res.send("Hello World!")
+});
+
+app.post("/login", (req,res)=>{
+    const loginEmail = req.body.userName;
+    console.log(JSON.stringify(req.body));
+    console.log("loginEmail", loginEmail);
+    const loginPassword = req.body.password;
+    console.log("loginPassword", loginPassword);
+    
+
+    if (loginEmail == "bigdaddy@yurmum.com" && loginPassword == "P@ssw0rd"){
+        const token = uuidv4();
+        res.send(token);
+    } else{
+        res.status(401);//unauthorized
+        res.send("Invalid user or password");
+    }
+})
