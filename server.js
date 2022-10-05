444353e0fcd5716bba01c66b81a7868c2447313a
Jackson Kilpack <18jkilpack@gmail.com>
2022-10-05 13:32:21 -0600

    updated server.js

--- server.js
+++ server.js
@@ -1,47 +1,53 @@
 const express = require("express");
 const bodyParser = require("body-parser")
-const {v4 : uuidv4} = require ("uuid");
+const { v4: uuidv4 } = require("uuid");
 const port = 3000;
 const app = express();
-const {createClient} = require('redis');
+const { createClient } = require('redis');
 const md5 = require('md5');
 const redisClient = createClient(
     {
-    Url:'redis://default@localhost:6379',
+        Url: 'redis://default@localhost:6379',
     }
 );
 
 app.use(bodyParser.json());
 
-app.listen(port, async ()=>{
+app.listen(port, async () => {
     await redisClient.connect();
-    console.log('Listening on port: ',port);
+    console.log('Listening on port: ', port);
 });
 
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.send("Hello World!")
 });
 
-app.post('/user',(req,res)=>{
+app.post('/user', (req, res) => {
     const newUserRequestObject = req.body;
     console.log('New User:', JSON.stringify(newUserRequestObject))
-    redisClient.hSet('users',req.body.email,JSON.stringify(newUserRequestObject));
-    res.send('New user'+newUserRequestObject.email+' added');
+    redisClient.hSet('users', req.body.email, JSON.stringify(newUserRequestObject));
+    res.send('New user' + newUserRequestObject.email + ' added');
 });
 
-app.post("/login", (req,res)=>{
+app.post("/login", async (req, res) => {
+
     const loginEmail = req.body.userName;
     console.log(JSON.stringify(req.body));
     console.log("loginEmail", loginEmail);
     const loginPassword = req.body.password;
     console.log("loginPassword", loginPassword);
-    
 
-    if (loginEmail == "bigdaddy@yurmum.com" && loginPassword == "P@ssw0rd"){
+    const userString = await redisClient.hGet('users', loginEmail);
+    const userObject = JSON.parse(userString)
+    if (userString == ' ' || userstring == null) {
+        res.status(404);
+        res.send('User not found');
+    }
+    else if (loginEmail == userObject.userName && loginPassword == userObject.password) {
         const token = uuidv4();
         res.send(token);
-    } else{
+    } else {
         res.status(401);//unauthorized
         res.send("Invalid user or password");
     }
