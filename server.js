2650639be98d26e7a3872683a37b6b918491b64d
Jackson Kilpack <18jkilpack@gmail.com>
2022-11-02 12:50:49 -0600

    updated server and docker

--- server.js
+++ server.js
@@ -1,72 +1,61 @@
-const express = require("express");
-const bodyParser = require("body-parser")
-const { v4: uuidv4 } = require("uuid");
+const express = require('express');
 const https = require('https');
 const fs = require('fs');
-const port = 443 //3000;
+const bodyParser = require('body-parser');
+const {v4 : uuidv4} = require('uuid');
+const port = 4043;
 const app = express();
-const { createClient } = require('redis');
+const {createClient} = require('redis');
 const md5 = require('md5');
+const { json } = require('body-parser');
+
 const redisClient = createClient(
-    {
-        Url: 'redis://default@localhost:6379',
-    }
+{
+      url:'redis://default@localhost:6379',
+}
 );
 
 app.use(bodyParser.json());
 
-// app.listen(port, async () => {
-//     await redisClient.connect();
-//     console.log('Listening on port: ', port);
-// });
-
 https.createServer({
     key: fs.readFileSync('server.key'),
     cert: fs.readFileSync('server.cert'),
-    ca: fs.readFileSync('chain.pem')
-    // passphrase: 'P@ssw0rd'
-}, app).listen(port, async () => {
-    console.log('Listening...')
+    ca:fs.readFileSync('chain.pem');
+}, app).listen(port, async ()=>{
+    await redisClient.connect();//creating a TCP socket with Redis
+    console.log("Listening on port: "+port);
+})
+
+app.get('/',(req,res)=>{
+    res.send('Hello Sean!');
 });
 
-
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-});
-
-app.post('/user', (req, res) => {
+app.post('/user',(req,res)=>{
     const newUserRequestObject = req.body;
-    const loginPassword = req.body.password;
-    const hash = md5(loginPassword);
-    console.log(hash);
-    newUserRequestObject.password=hash;
-    newUserRequestObject.verifyPassword=hash;
-    console.log('New User:', JSON.stringify(newUserRequestObject));
-    redisClient.hSet('users', req.body.email, JSON.stringify(newUserRequestObject));
-
-    res.send('New user' + newUserRequestObject.email + ' added');
+    console.log('New User:',JSON.stringify(newUserRequestObject));
+    redisClient.hSet('users',req.body.email,JSON.stringify(newUserRequestObject));
+    res.send('New user'+newUserRequestObject.email+' added');
 });
 
-app.post("/login", async (req, res) => {
-
+app.post('/login', async (req,res)=>{
     const loginEmail = req.body.userName;
     console.log(JSON.stringify(req.body));
-    console.log("loginEmail", loginEmail);
+    console.log('loginEmail',loginEmail);
     const loginPassword = req.body.password;
-    const hash = md5(loginPassword);
-    console.log("loginPassword", loginPassword);
+    console.log('loginPasword',loginPassword);
 
-    const userString = await redisClient.hGet('users', loginEmail);
-    const userObject = JSON.parse(userString)
-    if (userString == ' ' || userString == null) {
+    const userString=await redisClient.hGet('users',loginEmail);
+    const userObject = JSON.parse(userString);
+    if(userString=='' || userString==null){
         res.status(404);
         res.send('User not found');
     }
-    else if (loginEmail == userObject.userName && hash == userObject.password) {
+    else if (loginEmail==userObject.userName && loginPassword==userObject.password){
         const token = uuidv4();
         res.send(token);
-    } else {
+    } else{
         res.status(401);//unauthorized
-        res.send("Invalid user or password");
+        res.send('Invalid user or password');
     }
-})
+
+});
